6967d56256a66234d02fe8055b09f2f4e1d69d0f
Vladimir Sobornov <thecodegod1@gmail.com>
2022-11-09 21:39:06 +0300

    online stock span

---
+++ src/main/java/solutions/n901_Online_Stock_Span/StockSpanner.java
@@ -0,0 +1,52 @@
+package solutions.n901_Online_Stock_Span;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author : Sobornov Vladimir
+ * @since : 09.11.2022
+ *
+ * Design an algorithm that collects daily price quotes for some stock
+ * and returns the span of that stock's price for the current day.
+ *
+ * The span of the stock's price today is defined as the maximum number of
+ * consecutive days (starting from today and going backward) for which the stock price
+ * was less than or equal to today's price.
+ **/
+public class StockSpanner {
+
+    private final Deque<Result> stocks = new ArrayDeque<>();
+
+
+    public StockSpanner() {
+
+    }
+
+    public int next(int price) {
+        int countDays = 1;
+
+        while (!stocks.isEmpty() && stocks.peekLast().price <= price)
+            countDays += stocks.pollLast().span;
+
+        stocks.addLast(new Result(price, countDays));
+
+        return countDays;
+    }
+
+    private class Result {
+
+        Result (int price, int span) {
+            this.price = price;
+            this.span = span;
+        }
+        private final int price;
+        private final int span;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
---
+++ src/test/java/solutions/StockSpannerTest.java
@@ -0,0 +1,53 @@
+package solutions;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import solutions.n901_Online_Stock_Span.StockSpanner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author : Sobornov Vladimir
+ * @since : 09.11.2022
+ **/
+public class StockSpannerTest {
+
+    private static StockSpanner stockSpanner;
+
+    int[] testStocks = {100,80,60,70,60,75,85};
+    int[] testComplexStocks = {31,41,48,59,79};
+
+    public StockSpannerTest() {
+
+    }
+
+    @Before
+    public void createNewStockSpanner() {
+        stockSpanner = new StockSpanner();
+    }
+
+    @Test
+    public void nextTest() {
+        Assert.assertEquals(1, stockSpanner.next(100));
+    }
+
+    @Test
+    public void nextArrayTest() {
+        List<Integer> expectedSpans = new ArrayList<>(Arrays.asList(1,1,1,2,1,4,6));
+        List<Integer> actualSpans = new ArrayList<>();
+        Arrays.stream(testStocks).forEach(e -> actualSpans.add(stockSpanner.next(e)));
+        Assert.assertEquals(expectedSpans, actualSpans);
+    }
+
+    @Test
+    public void nextComplexArrayTest() {
+        List<Integer> expectedSpans = new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        List<Integer> actualSpans = new ArrayList<>();
+        Arrays.stream(testComplexStocks).forEach(e -> actualSpans.add(stockSpanner.next(e)));
+        Assert.assertEquals(expectedSpans, actualSpans);
+    }
+}
